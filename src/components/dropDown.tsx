121dee8a4f0f1bd98005e4b0cbc140aee2592215
leavemeal0n3 <zhuniskhan.azat@gmail.com>
2024-03-06 04:35:39 +0500

    change button to all div

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 const Dropdown = () => {
   return (
-    <div className="bg-third flex items-center justify-between w-full h-[50px] rounded-[10px] p-5">
-      <p className="font-inter text-base font-semibold text-low">
-        Place of Study
-      </p>
-      <button className="flex items-center">
+    <div className="bg-third flex w-full h-full rounded-[10px]">
+      <button className="flex w-full p-3 justify-between items-center">
+        <p className="font-inter text-base font-semibold text-low">
+          Place of Study
+        </p>
         <Image src="/arrowDown.svg" alt="arrowDown" width={14} height={8} />
       </button>
     </div>
